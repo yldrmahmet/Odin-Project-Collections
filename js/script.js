6c78f9702dcc488db8b557e885379c00ee306c2b
yldrmahmet <ay689089@gmail.com>
2025-10-28 00:52:51 +0300

    Exercise javascript functions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let a = 10;
+console.log(a);
+a = 20;
+console.log(a);
+
+let b = 7 * a;
+console.log(b);
+
+alert(name + surname);
