ec643a4936ec44c76537795659cc51a6bec27634
yldrmahmet <ay689089@gmail.com>
2025-11-15 15:05:36 +0300

    Move foundations path files child folder

--- js/functions/defaultValues.js
+++
@@ -1,7 +0,0 @@
-function showMessage(from, text = "no text given") {
-  console.log(from + ": " + text);
-}
-
-showMessage("Ann"); // Ann: no text given
-
-showMessage("Ann", undefined); // Ann: no text given
--- js/functions/minNumber.js
+++
@@ -1,8 +0,0 @@
-const min = (number1, number2) => {
-  return number1 > number2 ? number2 : number1;
-};
-
-console.log(min(2, 5));
-console.log(min(10, 8));
-console.log(min(3, 3));
-console.log(min(2, -123));
